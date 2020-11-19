a8b08d93460064284f43d5727191949e3913ad1d
Vladislav Moskvichev <vmoskvichev.exceedteam@gmail.com>
2020-11-19 17:22:29 +0300

    login form has done

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,20 +2,30 @@ import React, {useState} from 'react'
 import './Login.css';
 
 const Login = () => {
-    const [login, setLogin] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const logIn = (e) => {
-        e.preventDefault();
-        localStorage.setItem('token', '1');
-        return
+    const fetchToken = async () => {
+        const res = await fetch('http://localhost:4000/auth', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        });
+
+        const {token} = await res.json()
+        localStorage.setItem('token', token);
     }
 
     return (
-        <form onSubmit={logIn} className="login-form">
-            <label htmlFor="login">
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            fetchToken()
+        }} className="login-form">
+            <label htmlFor="email">
                 <span>Login</span>
-                <input className="input login" value={login} onChange={(e) => setLogin(e.target.value)} type="text"/>
+                <input className="input login" value={email} onChange={(e) => setEmail(e.target.value)} type="text"/>
             </label>
 
             <label htmlFor="password">
